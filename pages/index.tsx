3ab092fb0448e2fd8719c361c86254c2c6e6e392
daichi-sato-design <daichisato.design@gmail.com>
2021-08-29 23:57:38 +0900

    section-tagの更新

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Layout } from '../components/Layout'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -17,22 +18,32 @@ export default function Home() {
         </div>
         <section className="about">
           <div className="about__wrap">
-            <div className="about__hd">
-              <div className="section-tag">
-                <div className="section-tag__num">1</div>
-                <div className="section-tag__ttl">Introduction</div>
-              </div>
-              <h3 className="ttl">
-                小さな目標達成を<br/>
-                日々積み重ね、<br/>
-                継続と信念で<br/>
-                夢へと歩を進める。
-              </h3>
+            <div className="section-tag">
+              <div className="section-tag__num">1</div>
+              <div className="section-tag__ttl">Introduction</div>
             </div>
-            <div className="about__body">
-              <p className="txt">
-                新しい時代の「経済性」と「社会性」が共存する豊かな循環を生み出す。WHEREのすべての活動は、その地づくりのために存在しています。「LOCAL LETTER」「SHARE by WHERE」「一棟貸し古民家 まつや邸」。人と地域、地域と企業をつなぎながら、共にあるべき未来を思考し、カタチにする。人と経済の持続的な循環を創出する、私たちの主な事業をご紹介します。
-              </p>
+            <div className="about__contents">
+              <div className="about__contents__hd">
+                <h3 className="ttl">
+                  小さな目標達成を<br/>
+                  日々積み重ね、<br/>
+                  継続と信念で<br/>
+                  夢へと歩を進める。
+                </h3>
+              </div>
+              <div className="about__contents__body">
+                <p className="txt">
+                  新しい時代の「経済性」と「社会性」が共存する豊かな循環を生み出す。WHEREのすべての活動は、その地づくりのために存在しています。「LOCAL LETTER」「SHARE by WHERE」「一棟貸し古民家 まつや邸」。人と地域、地域と企業をつなぎながら、共にあるべき未来を思考し、カタチにする。人と経済の持続的な循環を創出する、私たちの主な事業をご紹介します。
+                </p>
+                <div className="section-btn">
+                  <Link href="/about">
+                    <a className="link">
+                      <span className="txt">About</span>
+                      <div className="deco"></div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </section>
