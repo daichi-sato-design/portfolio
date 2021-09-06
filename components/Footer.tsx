4d4c19bf31d5cbc4b8f623056e364a843c2f390c
daichi-sato-design <daichisato.design@gmail.com>
2021-09-06 22:46:04 +0900

    FooterにTopScrollを設置

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -78,6 +78,18 @@ const Footer:VFC = () => {
           </ul>
         </nav>
       </div>
+      <div className="footer__btn">
+        <a href="#container" className="footer__btn__wrap">
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path fillRule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clipRule="evenodd" />
+          </svg>
+        </a>
+      </div>
     </footer>
   )
 }
