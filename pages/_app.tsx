3ac0f111f94facb78c44d0f253a57225f6c5c9f2
daichi-sato-design <daichisato.design@gmail.com>
2021-09-08 01:05:59 +0900

    appをシンプルに

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,9 @@
 import '../styles/globals.scss'
 
-import { VFC, useEffect } from 'react'
-import Rellax from 'rellax'
+import { VFC } from 'react'
 import { AppProps } from 'next/app'
 
 const MyApp: VFC = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    //default JS Setting
-    var rellax = new Rellax('.rellax', {
-      breakpoints: [480, 768, 1080],
-    })
-  }, [])
   return <Component {...pageProps} />
 }
 
