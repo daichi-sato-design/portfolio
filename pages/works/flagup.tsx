d5f5e2d62873828c732c82d7e8bbde52c6836a9d
daichi-sato-design <daichisato.design@gmail.com>
2021-11-28 23:45:44 +0900

    work_dページ作成

--- pages/works/flagup.tsx
+++ pages/works/flagup.tsx
@@ -12,9 +12,15 @@ const Flagup: VFC = () => {
     <Layout title="DaichiSato Portfolio | Flagup">
       <main className="l-page works_d">
         <div className="mv">
-          <div className="mv__wrap">
-            <h2 className="mv__ttl">Flagup</h2>
-            <p className="mv__category">モバイルアプリ</p>
+          <div className="mv__visual">
+            <div className="mv__visual__wrap">
+              <Image
+                src="/works/flagup_main.jpg"
+                alt="flagup"
+                width={1100}
+                height={560}
+              />
+            </div>
           </div>
           <div className="mv__bg01 rellax" data-rellax-speed="-2.75">
             <Image
@@ -33,14 +39,113 @@ const Flagup: VFC = () => {
             />
           </div>
         </div>
-        <div className="visual">
-          <div className="visual__wrap">
-            <Image
-              src="/works/flagup_main.jpg"
-              alt="flagup"
-              width={1100}
-              height={560}
-            />
+        <div className="contents px-50">
+          <div className="contents__rows">
+            <div className="detail">
+              <div className="detail__hd">
+                <div className="detail__hd__ttl">
+                  <h2 className="ttl">Flagup</h2>
+                  <span className="category">SNS</span>
+                </div>
+                <p className="detail__hd__concept">
+                  情報収集をもっと効率的かつ専門的に。
+                </p>
+              </div>
+              <ul className="detail__block">
+                {/* <li className="detail__block__item">
+                  <h3 className="ttl">サービス紹介</h3>
+                  <div className="body">
+                    <p className="body__txt">
+                      本サイト・サービス・メディアなどに関するご意見やお問い合わせなどに対応いたします。お問い合わせに関するレスポンスが遅い場合、Twitterなど他メディアでのDMもご検討ください。お問い合わせに早急に対応できない場合もあります。
+                    </p>
+                  </div>
+                </li> */}
+                {/* <li className="detail__block__item">
+                  <h3 className="ttl">制作の目的</h3>
+                  <div className="body">
+                    <p className="body__txt">
+                      本サイト・サービス・メディアなどに関するご意見やお問い合わせなどに対応いたします。お問い合わせに関するレスポンスが遅い場合、Twitterなど他メディアでのDMもご検討ください。お問い合わせに早急に対応できない場合もあります。
+                    </p>
+                  </div>
+                </li> */}
+                {/* <li className="detail__block__item">
+                  <h3 className="ttl">工夫やこだわり</h3>
+                  <div className="body">
+                    <p className="body__txt">
+                      本サイト・サービス・メディアなどに関するご意見やお問い合わせなどに対応いたします。お問い合わせに関するレスポンスが遅い場合、Twitterなど他メディアでのDMもご検討ください。お問い合わせに早急に対応できない場合もあります。
+                    </p>
+                  </div>
+                </li> */}
+                <li className="detail__block__item">
+                  <h3 className="ttl">使用した技術</h3>
+                  <ul className="skill-list">
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://dart.dev/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/dart.png"
+                          alt="Dart"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://flutter.dev/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/flutter.png"
+                          alt="Flutter"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://firebase.google.com/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/firebase.png"
+                          alt="Firebase"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div className="info px-50">
+              <ul className="info__block">
+                <li className="info__block__item">
+                  <h3 className="ttl">日付</h3>
+                  <p className="body">2021.05.01</p>
+                </li>
+                <li className="info__block__item">
+                  <h3 className="ttl">仕様</h3>
+                  <p className="body">モバイルアプリ</p>
+                </li>
+              </ul>
+              <div className="stopping-btn">
+                <span className="txt">配信を停止しています</span>
+              </div>
+            </div>
           </div>
         </div>
       </main>
--- pages/works/ptybo.tsx
+++ pages/works/ptybo.tsx
@@ -9,12 +9,18 @@ const Ptybo: VFC = () => {
     var rellax = new Rellax('.rellax')
   }, [])
   return (
-    <Layout title="DaichiSato Portfolio | Flagup">
+    <Layout title="DaichiSato Portfolio | Ptybo">
       <main className="l-page works_d">
         <div className="mv">
-          <div className="mv__wrap">
-            <h2 className="mv__ttl">Ptybo</h2>
-            <p className="mv__category">Webサービス</p>
+          <div className="mv__visual">
+            <div className="mv__visual__wrap">
+              <Image
+                src="/works/ptybo_main.jpg"
+                alt="flagup"
+                width={1100}
+                height={560}
+              />
+            </div>
           </div>
           <div className="mv__bg01 rellax" data-rellax-speed="-2.75">
             <Image
@@ -33,28 +39,216 @@ const Ptybo: VFC = () => {
             />
           </div>
         </div>
-        <div className="visual">
-          <div className="visual__wrap">
-            <Image
-              src="/works/ptybo_main.jpg"
-              alt="ptybo"
-              width={1100}
-              height={560}
-            />
-          </div>
-        </div>
-        <div className="info px-50">
-          <div className="info__wrap">
-            <div className="section-btn">
-              <a
-                href="https://ptybo.online"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="link"
-              >
-                <span className="txt">View more</span>
-                <div className="deco"></div>
-              </a>
+        <div className="contents px-50">
+          <div className="contents__rows">
+            <div className="detail">
+              <div className="detail__hd">
+                <div className="detail__hd__ttl">
+                  <h2 className="ttl">Ptybo</h2>
+                  <span className="category">マッチングサービス</span>
+                </div>
+                <p className="detail__hd__concept">
+                  今すぐ遊べるプレイヤーを探すゲーマーのパーティー募集サービス
+                </p>
+              </div>
+              <ul className="detail__block">
+                {/* <li className="detail__block__item">
+                  <h3 className="ttl">サービス紹介</h3>
+                  <div className="body">
+                    <p className="body__txt">
+                      本サイト・サービス・メディアなどに関するご意見やお問い合わせなどに対応いたします。お問い合わせに関するレスポンスが遅い場合、Twitterなど他メディアでのDMもご検討ください。お問い合わせに早急に対応できない場合もあります。
+                    </p>
+                  </div>
+                </li> */}
+                {/* <li className="detail__block__item">
+                  <h3 className="ttl">制作の目的</h3>
+                  <div className="body">
+                    <p className="body__txt">
+                      本サイト・サービス・メディアなどに関するご意見やお問い合わせなどに対応いたします。お問い合わせに関するレスポンスが遅い場合、Twitterなど他メディアでのDMもご検討ください。お問い合わせに早急に対応できない場合もあります。
+                    </p>
+                  </div>
+                </li> */}
+                {/* <li className="detail__block__item">
+                  <h3 className="ttl">工夫やこだわり</h3>
+                  <div className="body">
+                    <p className="body__txt">
+                      本サイト・サービス・メディアなどに関するご意見やお問い合わせなどに対応いたします。お問い合わせに関するレスポンスが遅い場合、Twitterなど他メディアでのDMもご検討ください。お問い合わせに早急に対応できない場合もあります。
+                    </p>
+                  </div>
+                </li> */}
+                <li className="detail__block__item">
+                  <h3 className="ttl">使用した技術</h3>
+                  <ul className="skill-list">
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://developer.mozilla.org/ja/docs/Web/JavaScript"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/javascript.png"
+                          alt="JaveScript"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://www.typescriptlang.org/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/typescript.png"
+                          alt="TypeScript"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://ja.reactjs.org/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/react.png"
+                          alt="React"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://nextjs.org/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/nextjs.png"
+                          alt="Next.js"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://tailwindcss.com/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/tailwindcss.png"
+                          alt="Tailwind CSS"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://golang.org/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/golang.png"
+                          alt="Golang"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://www.mongodb.com/atlas"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/mongodb.png"
+                          alt="MongoDB"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://cloud.google.com/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/gcp.png"
+                          alt="Google Cloud Platform"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                    <li className="skill-list__item">
+                      <a
+                        className="link"
+                        href="https://jwt.io/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <Image
+                          loading="lazy"
+                          src="/skills/jwt.png"
+                          alt="jwt"
+                          width={135}
+                          height={135}
+                        />
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div className="info px-50">
+              <ul className="info__block">
+                <li className="info__block__item">
+                  <h3 className="ttl">日付</h3>
+                  <p className="body">2021.10.01</p>
+                </li>
+                <li className="info__block__item">
+                  <h3 className="ttl">仕様</h3>
+                  <p className="body">WEBサービス</p>
+                </li>
+              </ul>
+              <div className="section-btn">
+                <a
+                  href="https://ptybo.online/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  <span className="txt">制作サイトへ</span>
+                  <div className="deco"></div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
